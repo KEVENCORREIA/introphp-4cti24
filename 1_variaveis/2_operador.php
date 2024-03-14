3b59aa95c420fac242249b8cf03d4dd89085f6df
KEVENCORREIA <125514343+KEVENCORREIA@users.noreply.github.com>
2024-03-13 21:09:08 -0400

    Add files via upload

---
+++ 1_variaveis/2_operador.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = 24;
+$b = 16;
+
+$c= $a + $b;  //soma
+echo '<br> A soma é ' . $c;
+
+$d = $a * $b;
+echo '<br> a multiplicacao é ' . $d;
+
+$d = $a / $b;
+echo '<br> a divisão é ' . $d;
+ 
+$d = $a - $b;
+echo '<br> a subtracao é ' . $d;
+
+
+
+?>
