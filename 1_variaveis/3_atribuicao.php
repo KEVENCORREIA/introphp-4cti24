516f7d94c11d52e4e2c53c0630af969a8c8c7669
KEVENCORREIA <125514343+KEVENCORREIA@users.noreply.github.com>
2024-04-05 21:15:58 -0300

    Add files via upload

---
+++ 1_variaveis/3_atribuicao.php
@@ -0,0 +1,21 @@
+<?php
+
+$a = 1;
+$a += 2;
+echo $a;
+
+echo '<hr>';
+$a= 4;
+$a /= 2;
+echo $a;
+
+echo'<hr>';
+$a= 4;
+$a /= 2;
+echo $a;
+echo'<hr>';
+
+$a = 32;
+$a -= 18;
+echo $a;
+?>
---
+++ 3_laco_repeticao/3_for.php
@@ -0,0 +1,16 @@
+<?php 
+
+/*inicio do loop - condição - <contador> */
+ for ($a = 1; $a <=10; $a++){
+     $cubo = $a * $a * $a;
+     echo "O cubo de $a é $cubo <br>";
+ }
+ 
+ for ($soma = 1; $soma <= 300; $soma++){
+     $quantos = $soma + 1;
+     echo "Fala 300! contra $soma <br>";
+
+
+ }
+
+?>
