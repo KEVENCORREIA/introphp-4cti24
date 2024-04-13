79edbb0b9c58b6791507f7534c88bdbfe2122ee1
KEVENCORREIA <125514343+KEVENCORREIA@users.noreply.github.com>
2024-04-12 21:09:03 -0300

    Add files via upload

---
+++ 3_laco_repeticao/4_forech.php
@@ -0,0 +1,29 @@
+<?php  
+
+$mendes = array ("Xandão é o cara!", "xanxan", "Bora BGS");
+
+foreach ($mendes as $view){
+echo $view;
+
+}
+
+?>
+<hr>
+<?php
+
+
+$meses = array(
+
+    'janeiro' => 1, 
+    'fevereiro' =>2,
+    'março' => 3,
+    'abril' => 4
+);
+
+foreach($meses as $chave => $valor){
+
+echo 'O mês ' . $chave . 'é' . $valor . '<br>';
+
+}
+
+?>
